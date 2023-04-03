eb98d72d4887de6b6b4f86c8c1cb969e4bfbc13c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 10:43:54 -0300

    aula-js6

---
+++ script.js
@@ -0,0 +1,18 @@
+//switch case
+
+let opcao = parseInt(prompt("Digite uma opção"));
+
+switch(opcao){
+    case 1:
+        document.write("Primeira opção")
+    break;
+    case 2:
+        document.write("Segunda opção")
+    break;
+    case 3:
+        document.write("Terceira opção")
+    break;
+    default:
+        document.write("Nehuma das opções válidas")
+    break;
+}
---
+++ script1.js
@@ -0,0 +1,6 @@
+ //escreve na tela 
+ document.write("informações para tela <br>")
+ document.write("meu primeiro Java Script na tela <br>")
+ document.write("Tela inicial do meu jogo <br>")
+ //alerta
+ alert("clique aqui")
