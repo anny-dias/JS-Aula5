f1a9ae03c5fd0f8f1fc8f5b772155c2f5f644dff
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 11:20:35 -0300

    aula

---
+++ script.js
@@ -0,0 +1,56 @@
+//switch case
+
+/*
+let opcao = parseInt(prompt("Digite uma opção"));
+
+switch(opcao){
+    case 1:
+        document.write("Primeira opção")
+    break;
+    case 2:
+        document.write("Segunda opção")
+    break;
+    case 3:
+        document.write("Terceira opção")
+    break;
+    default:
+        document.write("Nehuma das opções válidas")
+    break;
+}
+*/
+
+
+//incremento(++) e decremento(--)
+
+/*
+let numero = 7
+document.write(numero)
+document.write("<br></br>")
+numero = numero+1
+document.write(numero)
+document.write("<br></br>")
+
+let numero1 = 7
+document.write(numero1)
+document.write("<br></br>")
+numero1 = numero1-1
+document.write(numero1)
+document.write("<br></br>")
+*/
+
+
+
+//for = para
+
+/*
+for(let i=0; i<10; i++){
+    document.write("Agora i vale " +i);
+    document.write("<br></br>")
+}
+*/
+
+let carros = ["Jeep","Compass","Hb20","HRV"]
+for(let i=0; i < carros.length; i++){
+    document.write(carros[i])
+    document.write("<br></br>")
+}
